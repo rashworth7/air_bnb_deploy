61d2ffe0bb7db96bad05130b57336874264e0f8f
charlottemothersole <charlottemothersole@gmail.com>
2023-07-18 15:46:43 +0100

    add methods and tests

--- lib/tenant_repository.py
+++ lib/tenant_repository.py
@@ -11,3 +11,9 @@ class TenantRepository():
             item = Tenant(row['id'], row['username'])
             tenants.append(item)
         return tenants
+    
+    def get_tenant_by_id(self, id):
+        rows = self._connection.execute('SELECT * from tenants where id=%s', [id])
+        row = rows[0]
+        tenant = Tenant(row['id'], row['username'])
+        return tenant
