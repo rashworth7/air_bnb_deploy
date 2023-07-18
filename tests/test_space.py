253f4343bc8d8a0f47eb1a8e9b5f26958584c84d
Oliver Loftus <oliverloftus1@gmail.com>
2023-07-18 16:37:44 +0100

    completed space model and tests, working on space_repository

--- tests/test_space.py
+++ tests/test_space.py
@@ -5,21 +5,20 @@ test constructs
 """
 
 def test_space_constructs():
-    space = Space(1, 'test title', 'test description', 'test price', 'test landlord id', 'test availability')
+    space = Space(1, 'test title', 'test description', 'test price', 'test landlord id')
     assert space.id == 1
     assert space.title == 'test title'
     assert space.description == 'test description'
     assert space.price_per_night == 'test price'
     assert space.landlord_id == 'test landlord id'
-    assert space.availability_dates == 'test availability'
 
 """
 test equality
 """
 
 def test_space_equality():
-        space1 = Space(1, 'test title', 'test description', 'test price', 'test landlord id', 'test availability')
-        space2 = Space(1, 'test title', 'test description', 'test price', 'test landlord id', 'test availability')
+        space1 = Space(1, 'test title', 'test description', 'test price', 'test landlord id')
+        space2 = Space(1, 'test title', 'test description', 'test price', 'test landlord id')
         assert space1 == space2
 
 """
@@ -27,5 +26,6 @@ formats nicely
 """
 
 def test_format_to_string():
-      space = Space(1, 'test title', 'test description', 'test price', 'test landlord id', 'test availability')
-      assert str(space) == "Space(1, test title, test description, test price, test landlord id, test availability)"
+    space = Space(1, 'test title', 'test description', 'test price', 'test landlord id')
+    assert str(space) == "Space(1, test title, test description, test price, test landlord id)"
+
--- tests/test_space_repository.py
+++ tests/test_space_repository.py
@@ -1,1 +1,35 @@
+from lib.space import Space 
 from lib.space_repository import SpaceRepository
+
+"""
+when we call the all method we get a list of space objects(db starts empty)
+"""
+def test_all_method(db_connection):
+    db_connection.seed("seeds/airbnb_seeds.sql")
+    repository = SpaceRepository(db_connection)
+
+    spaces = repository.all()
+
+    assert spaces == [
+        Space(1, 'Space 1', 'Space 1 is very nice', 50, 1),
+        Space(2,'Space 2', 'Space 2 is cool', 60, 1),
+        Space(3,'Space 3', 'Space 3 rubbish', 20, 2)
+    ]
+
+"""
+when we call create we get a new space in the list
+"""
+def test_create_method(db_connection):
+    db_connection.seed("seeds/airbnb_seeds.sql")
+    repository = SpaceRepository(db_connection)
+
+    repository.create_space(Space(None, 'Cozy Space', 'Coziest space ever!', 120, 3))
+
+    spaces = repository.all()
+
+    assert spaces == [
+                Space(1, 'Space 1', 'Space 1 is very nice', 50, 1),
+                Space(2,'Space 2', 'Space 2 is cool', 60, 1),
+                Space(3,'Space 3', 'Space 3 rubbish', 20, 2),
+                Space(4, 'Cozy Space', 'Coziest space ever!', 120, 3)
+    ]
